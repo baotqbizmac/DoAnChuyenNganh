a61188c24aa6dd8e6e5ca478bcfa03ece654d693
Tran Quoc Bao <bao.tq@bizmac.com.vn>
2018-10-26 10:46:53 +0700

    Lay cau hoi

---
+++ DoAnChuyenNganh_AiLaTrieuPhu/DoAnChuyenNganh_AiLaTrieuPhu/Models/QuestionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DoAnChuyenNganh_AiLaTrieuPhu.Models
+{
+    public class QuestionModel
+    {
+        public string Question { get; set; }
+        public List<string> Answer { get; set; } = new List<string>();
+        public string CorrectAnswer { get; set; }
+    }
+}
---
+++ DoAnChuyenNganh_AiLaTrieuPhu/DoAnChuyenNganh_AiLaTrieuPhu/Service/XmlDataReader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml;
+using System.Xml.Linq;
+using DoAnChuyenNganh_AiLaTrieuPhu.Models;
+using Newtonsoft.Json;
+
+namespace DoAnChuyenNganh_AiLaTrieuPhu.Service
+{
+    public class XmlDataReader
+    {
+        private List<QuestionModel> Questions { get; set; }
+        public List<QuestionModel> GetQuestion(int level)
+        {
+            Questions = new List<QuestionModel>();
+            //Get question from file xml
+            var filepath = AppDomain.CurrentDomain.BaseDirectory;
+            XDocument doc = XDocument.Load(filepath + @"/Data/XML_QuestionData.xml");
+            var question = doc.Descendants("Question");
+
+            foreach (var item in question)
+            {
+                QuestionModel Question = new QuestionModel();
+                Question.Question = item.LastAttribute.Value;
+                var lev = item.FirstAttribute.Value;
+
+                foreach (var i in item.Descendants("Ans"))
+                {
+                    if (i.Attribute("correct").Value == "true")
+                    {
+                        Question.CorrectAnswer = i.Value;
+                    }
+                    Question.Answer.Add(i.Value);
+                }
+                Questions.Add(Question);
+            }
+            return Questions;
+        }
+    }
+}
